400730dfa7f86726e2fa239951926a8772dbfcfc
PROPERTY\214843 <mintumia8@gmail.com>
2023-08-02 18:25:40 +0600

    Tailwind CSS Added

---
+++ resources/views/test.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New Page</title>
+    <link rel="stylesheet" href="{{asset('plugins')}}/tailwindcss/main.css">
+</head>
+<body>
+<h2 class="mt-6 bg-purple-700 text-white">Hello</h2>
+
+
+
+</body>
+</html>
